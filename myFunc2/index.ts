6697f862732c78ccf188bc3f40f72a2a760acd0d
SatakeYusuke19920527 <satkyusk@icloud.com>
2023-05-28 21:51:11 +0900

    commit

--- myFunc2/index.ts
+++ myFunc2/index.ts
@@ -1,17 +1,17 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context } from '@azure/functions';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const eventGridTrigger: AzureFunction = async function (
+  context: Context,
+  eventGridEvent: any
+): Promise<void> {
+  context.log('Event received:', eventGridEvent);
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  // イベントの処理ロジックをここに追加します
 
+  context.res = {
+    status: 200,
+    body: 'Event received',
+  };
 };
 
-export default httpTrigger;
+export default eventGridTrigger;
